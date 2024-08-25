58362913dbf981bcf93a5095c1f3fafe356d4108
since_2024 <99060899+since-2024@users.noreply.github.com>
2024-08-25 12:20:43 +0900

    [Bronze III] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int a, b;
+  cin >> a >> b;
+
+  cout << (b % 10) * a << endl;
+  cout << (b % 100) / 10 * a << endl;
+  cout << (b / 100) * a << endl;
+  cout << b * a << endl;
+
+  return 0;
+}
