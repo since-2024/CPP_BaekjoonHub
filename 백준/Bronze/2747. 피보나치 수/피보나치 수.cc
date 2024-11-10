044b2741dc52c145e2301208795e59ad8bd128f9
since_2024 <99060899+since-2024@users.noreply.github.com>
2024-11-10 23:04:34 +0900

    [Bronze II] Title: 피보나치 수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+int dp[46];
+
+int main()
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  cin >> n;
+
+  dp[1] = dp[2] = 1;
+
+  for (int i = 3; i <= n; i++)
+    dp[i] = dp[i-2] + dp[i-1];
+
+  cout << dp[n];
+}
