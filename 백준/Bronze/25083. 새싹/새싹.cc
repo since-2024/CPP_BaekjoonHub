a6ea9f72a4b2781fe1166fad5d11872e16adafb2
since_2024 <99060899+since-2024@users.noreply.github.com>
2024-12-19 08:17:07 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string s;
+
+int main()
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  
+  s = "         ,r'\"7\n";
+  s += "r`-_   ,'  ,/\n";
+  s += " \\. \". L_r'\n";
+  s += "   `~\\/\n";
+  s += "      \|\n";
+  s += "      \|";
+  
+  cout << s;
+}
