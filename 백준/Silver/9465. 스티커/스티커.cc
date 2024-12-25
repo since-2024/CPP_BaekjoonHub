89759fdd1e4ea9b0682add9e340bc6847d92f4ff
since_2024 <99060899+since-2024@users.noreply.github.com>
2024-12-25 22:13:47 +0900

    [Silver I] Title: 스티커, Time: 76 ms, Memory: 3460 KB -BaekjoonHub

---
+++ 백준/Silver/9465. 스티커/스티커.cc
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int t, n;
+
+
+int main()
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  cin >> t;
+
+  while (t--)
+  {
+    cin >> n;
+
+    int arr[2][n+1];
+    int dp[2][n+1];
+
+    for (int i = 0; i < 2; i++)
+      for (int j = 0; j < n; j++)
+        cin >> arr[i][j];
+
+    dp[0][0] = arr[0][0];
+    dp[1][0] = arr[1][0];
+    dp[0][1] = arr[0][1] + dp[1][0];
+    dp[1][1] = arr[1][1] + dp[0][0];
+
+    for (int i = 2; i < n; i++)
+    {
+      dp[0][i] = arr[0][i] + max(dp[1][i-1], dp[1][i-2]);
+      dp[1][i] = arr[1][i] + max(dp[0][i-1], dp[0][i-2]);
+    }
+
+    cout << max(dp[0][n-1], dp[1][n-1]) << "\n";
+  }
+}
